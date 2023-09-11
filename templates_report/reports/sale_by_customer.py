e15a9f3fe107fd56949c7bd8a1cb4749f159fe1d
Vanndy <sengsovanndy18@kit.edu.kh>
2023-09-11 16:45:45 +0700

    add date to daily sale by customer

--- templates_report/reports/sale_by_customer.py
+++ templates_report/reports/sale_by_customer.py
@@ -8,17 +8,22 @@ class SaleByCustomer(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         docs = self.env['account.move'].browse(docids)
         customer_group = {}
+        group_date = set()
         for account_move in docs:
             customer_name = account_move.partner_id.name
+            group_date.add(str(account_move.invoice_date))
             for order in account_move.invoice_line_ids:
                 if customer_name not in customer_group:
                     customer_group[customer_name] = [order]
                 else:
                     customer_group[customer_name].append(order)
-            print(customer_group)
+            # print(customer_group)
+            # print(group_date)
+
         return {
             'doc_ids': docids,
             'docs': docs,
             'data': data,
             'customer': customer_group,
+            'date': ", ".join(sorted(list(group_date))),
         }
